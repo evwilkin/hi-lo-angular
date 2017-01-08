754cca433ad0a10b6c8941791267098b90033a6d
evwilkin <evan.wilkinson@gmail.com>
2017-01-08 13:30:07 -0500

    added scoring notes and logic for scoring each guess

--- scratch.js
+++ scratch.js
@@ -3,40 +3,87 @@ $(document).ready(function() {
   
   //Declare variables
   var deckID,
+      cards = [ 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K'],
       guess,
+      answer,
       score1,
       score2,
-      $current = $("#current"),
-      $remaining = $("#remaining");
+      discard, //keep track of correct guesses (and points to award when wrong guess)
+      cardPic,
+      lastCard,
+      currentCard,
+      $currentImg = $("#current"),
+      $remaining = $("#remaining"),
+      $guess = $("#guess");
 
   //Start the game
   newGame();
   $("#newGame").on("click", newGame);
 
   //Draw a card
-  $(".next").on("click", drawCard);
+  $(".next").on("click", function() {
+  //$("#higher").on("click", function() {
+    guess = $(this).attr("id");
+    drawCard(guess);
+  });
 
   function newGame() {
     $.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', function(res) {
       deckID = res.deck_id;
       score1 = 0;
       score2 = 0;
+      discard=0;
+      lastCard = "";
+      currentCard = "";
+      $guess.text("");
       drawCard();
-      console.log(res);
     });
   }
 
-  function drawCard() {
+  function drawCard(guess) {
+    //Move current card to discard pile
+    lastCard = currentCard;
     //if guess then draw card, else prompt guess first
     $.get('https://deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=1', function(res) {
-      var cardPic = res.cards[0].image;
-      $current.html("<img src='" + cardPic + "' alt='playing card'/>");
+      cardPic = res.cards[0].image;
+      $currentImg.html("<img src='" + cardPic + "' alt='playing card'/>");
       $remaining.text(res.remaining);
+      currentCard = res.cards[0].code[0];
+      console.log(currentCard);
+      if (res.remaining < 51) {
+        $guess.text(guess);
+        checkAnswer(lastCard, currentCard, guess);
+      }
     });
   }
 
-  function checkAnswer() {
-    //
+  function checkAnswer(lastCard, currentCard, guess) {
+    console.log("last: " + lastCard + " current: " + currentCard + " guess: " + guess);
+    switch (guess) {
+      case "higher":
+        if (cards.indexOf(currentCard) > cards.indexOf(lastCard)) {
+          console.log("right guess!");
+        } else if (cards.indexOf(currentCard) < cards.indexOf(lastCard)) {
+          console.log("Sorry - this card is lower than the last!");
+        } else {
+          console.log("Same value card - pass.");
+        }
+        console.log(cards.indexOf(currentCard) + " " + cards.indexOf(lastCard));
+        break;
+      case "lower":
+        if (cards.indexOf(currentCard) < cards.indexOf(lastCard)) {
+          console.log("right guess!");
+        } else if (cards.indexOf(currentCard) > cards.indexOf(lastCard)) {
+          console.log("Sorry - this card is higher than the last!");
+        } else {
+          console.log("Same value card - pass.");
+        }
+        console.log(cards.indexOf(currentCard) + " " + cards.indexOf(lastCard));
+        break;
+      default:
+        console.log(cards.indexOf(currentCard) + " " + cards.indexOf(lastCard));
+        break;
+    }
   }
 
 });
