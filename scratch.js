83f50da45c150de663fce679f2dce72a71512a7c
evwilkin <evan.wilkinson@gmail.com>
2017-01-09 00:58:22 -0500

    added styling

--- scratch.js
+++ scratch.js
@@ -1,14 +1,14 @@
 //jQuery
 $(document).ready(function() {
-  
   //Declare variables
   var deckID,
-      cards = [ 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K'],
+      cards = [ 'ACE', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK', 'QUEEN', 'KING'],
       guess, //keep track of player's guess,
       correctGuesses,
       player1 = true,
       score1,
       score2,
+      hideLastCard = true,
       discard = 0, //keep track of correct guesses (and points to award when wrong guess)
       cardPic,
       lastCard,
@@ -16,22 +16,37 @@ $(document).ready(function() {
       $currentImg = $("#current"),
       $previousImg = $("#previous"),
       $remaining = $("#remaining"),
-      $guess = $("#guess"),
       $answer = $("#answer"),
       $discard = $("#discard"),
       $player1 = $("#player1"),
-      $player2 = $("#player2");
+      $player2 = $("#player2"),
+      $rules = $("#rules"),
+      $lastCard = $(".lastCard"),
+      $trade = $('#trade');
+
+  //Setup screen
+  $rules.hide();
+  $trade.hide();
+  $lastCard.hide();
+  $("h3").on("click", function() {
+    $rules.slideToggle();
+  });
 
   //Start the game
   newGame();
   $("#newGame").on("click", newGame);
 
   //Draw a card
-  $(".next").on("click", function() {
-  //$("#higher").on("click", function() {
+  $(".next:not(#trade)").on("click", function() {
     guess = $(this).attr("id");
+    if (hideLastCard) {
+      $lastCard.show();
+      hideLastCard = false;
+    }
     drawCard(guess);
   });
+
+  $(trade).on("click", changePlayer);
 
   function newGame() {
     //Draw a new deck and reset the variables
@@ -44,7 +59,8 @@ $(document).ready(function() {
       player1 = true;
       lastCard = "";
       currentCard = "";
-      $guess.text("");
+      $previousImg.html("");
+      $currentImg.html("");
       drawCard();
     });
   }
@@ -56,11 +72,10 @@ $(document).ready(function() {
     //if guess then draw card, else prompt guess first
     $.get('https://deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=1', function(res) {
       cardPic = res.cards[0].image;
-      $currentImg.html("<img src='" + cardPic + "' alt='playing card'/>");
+      $currentImg.html("<img src='" + cardPic + "' alt='playing card' />");
       $remaining.text(res.remaining);
-      currentCard = res.cards[0].code[0];
+      currentCard = res.cards[0].value;
       if (res.remaining < 51) {
-        $guess.text(guess);
         discard++;
         checkAnswer(lastCard, currentCard, guess, res.remaining);
       }
@@ -73,7 +88,7 @@ $(document).ready(function() {
         if (cards.indexOf(currentCard) > cards.indexOf(lastCard)) {
           $answer.text("You are correct - " + currentCard + " is higher than " + lastCard + "!");
           correctGuesses++;
-          if (correctGuesses >= 3) {
+          if (correctGuesses === 3) {
             changePlayer();
           }
         } else if (cards.indexOf(currentCard) < cards.indexOf(lastCard)) {
@@ -87,6 +102,9 @@ $(document).ready(function() {
           }
           correctGuesses = 0;
           discard = 0;
+          $lastCard.hide();
+          hideLastCard = true;
+          $trade.hide();
         } else {
           $answer.text("Same value card - pass.");
         }
@@ -96,7 +114,7 @@ $(document).ready(function() {
         if (cards.indexOf(currentCard) < cards.indexOf(lastCard)) {
           $answer.text("You are correct - " + currentCard + " is lower than " + lastCard + "!");
           correctGuesses++;
-          if (correctGuesses >= 3) {
+          if (correctGuesses === 3) {
             changePlayer();
           }
         } else if (cards.indexOf(currentCard) > cards.indexOf(lastCard)) {
@@ -110,6 +128,9 @@ $(document).ready(function() {
           }
           correctGuesses = 0;
           discard = 0;
+          $lastCard.hide();
+          $trade.hide();
+          hideLastCard = true;
         } else {
           $answer.text("Same value card - pass.");
         }
@@ -134,10 +155,16 @@ $(document).ready(function() {
       confirmButtonText: "Yes, let the other player guess!",
       closeOnConfirm: false
     },
-    function(){
-      player1 = !player1;
-      correctGuesses = 0;
-      swal("Turn changed!", "Sit back and enjoy - time for " + (player1 ? "player 1" : "player 2") + " to guess.", "success");
+    function(accept){
+      if (accept) {
+        player1 = !player1;
+        correctGuesses = 0;
+        $(trade).hide();
+        $(answer).html("");
+        swal("Turn changed!", "Sit back and enjoy - time for " + (player1 ? "player 1" : "player 2") + " to guess.", "success");
+      } else {
+        $trade.show();
+      }
     });
   }
 
@@ -152,17 +179,6 @@ $(document).ready(function() {
   }
 
 });
-
-
-
-/* Vanilla JS
-var xhr = new XMLHttpRequest();
-xhr.onload = function() {
-  console.log(xhr.responseText);
-};
-xhr.open('get', 'https://deckofcardsapi.com/api/deck/new/', false);
-xhr.send();
-*/
 
 /* Functions
 
